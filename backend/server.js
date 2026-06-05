f02363c035f864ee6c49919335a470c2ecb4721c
Anityasundar <anityasundar53@gmail.com>
2026-06-05 19:36:10 +0000

    code level changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const os = require("os");
+const path = require("path");   // ✅ ADD THIS
 
 const app = express();
 const PORT = 3000;
@@ -8,21 +9,21 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+/* ✅ ADD THIS LINE */
+app.use(express.static(path.join(__dirname, "frontend")));
+
 app.get("/", (req, res) => {
-    res.send("DevOps Backend Server Running 🚀");
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.get("/server-info", (req, res) => {
-
     const serverInfo = {
         hostname: os.hostname(),
         platform: os.platform(),
         architecture: os.arch(),
         uptime: Math.floor(os.uptime() / 3600) + " Hours",
-        totalMemory:
-            Math.round(os.totalmem() / (1024 * 1024 * 1024)) + " GB",
-        freeMemory:
-            Math.round(os.freemem() / (1024 * 1024 * 1024)) + " GB",
+        totalMemory: Math.round(os.totalmem() / (1024 * 1024 * 1024)) + " GB",
+        freeMemory: Math.round(os.freemem() / (1024 * 1024 * 1024)) + " GB",
         cpuCount: os.cpus().length
     };
 
